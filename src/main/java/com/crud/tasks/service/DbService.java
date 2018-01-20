eac0df54e07ac312512f169a23a3a1fd07c021ae
“Chupacabra1985 <jan.roztropinski@op.pl>
2018-01-20 16:49:23 +0100

    Test

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,7 +1,6 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.domain.Task;
-import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +12,10 @@ import java.util.Optional;
 public class DbService {
     @Autowired
     private TaskRepository repository;
+
+    public DbService(TaskRepository repository) {
+        this.repository = repository;
+    }
 
     public List<Task> getAllTasks(){
         return repository.findAll();
--- src/test/java/com/crud/tasks/controller/ControllerTestSuite.java
+++ src/test/java/com/crud/tasks/controller/ControllerTestSuite.java
@@ -3,12 +3,22 @@ package com.crud.tasks.controller;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.DbService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,14 +27,43 @@ public class ControllerTestSuite {
     private TaskController taskController;
 
     @Test
-    public void testGetTask(){
-
+    public void testGetTask() throws TaskNotFoundException {
         //Then
-        TaskDto readTask = taskController.getTask((long)1);
+        TaskDto readTask = taskController.getTask((long)2);
         long id = readTask.getId();
 
-        Assert.assertEquals(id, 1);
-        Assert.assertNotEquals(id, 0);
+        Assert.assertEquals(id, 2);
+
+    }
+
+    @Test
+    public void testDbService(){
+        //Given
+        TaskRepository repository = mock(TaskRepository.class);
+        DbService dbService = new DbService(repository);
+        List<Task> resultList = new ArrayList<Task>();
+        Task task1 = new Task((long)4, "MockTest", "TestMock");
+        Task task2 = new Task((long)5, "Abc", "Bcd");
+        Task task3 = new Task((long)6, "CVds", "ASFRhsk");
+
+        resultList.add(task1);
+        resultList.add(task2);
+        resultList.add(task3);
+
+        when(repository.findAll()).thenReturn(resultList);
+        when(repository.save(task1)).thenReturn(task1);
+        when(repository.findById(anyLong())).thenReturn(null);
+
+
+        //When
+        List<Task> resultGetTasks = dbService.getAllTasks();
+        Task resultSaveTask = dbService.saveTask(task1);
+        Optional<Task> resultGetTask = dbService.getTask((long)2);
+
+        //Then
+        Assert.assertEquals(3, resultGetTasks.size());
+        Assert.assertSame(task1, resultSaveTask);
+        Assert.assertNull(resultGetTask);
 
     }
 }
