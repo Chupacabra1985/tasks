0a3e353e5692b61e2de657fd81bb4024b332edfd
“Chupacabra1985 <jan.roztropinski@op.pl>
2018-01-21 19:54:13 +0100

    TestServiceNewClass

--- src/test/java/com/crud/tasks/controller/ControllerTestSuite.java
+++ src/test/java/com/crud/tasks/controller/ControllerTestSuite.java
@@ -36,34 +36,4 @@ public class ControllerTestSuite {
 
     }
 
-    @Test
-    public void testDbService(){
-        //Given
-        TaskRepository repository = mock(TaskRepository.class);
-        DbService dbService = new DbService(repository);
-        List<Task> resultList = new ArrayList<Task>();
-        Task task1 = new Task((long)4, "MockTest", "TestMock");
-        Task task2 = new Task((long)5, "Abc", "Bcd");
-        Task task3 = new Task((long)6, "CVds", "ASFRhsk");
-
-        resultList.add(task1);
-        resultList.add(task2);
-        resultList.add(task3);
-
-        when(repository.findAll()).thenReturn(resultList);
-        when(repository.save(task1)).thenReturn(task1);
-        when(repository.findById(anyLong())).thenReturn(null);
-
-
-        //When
-        List<Task> resultGetTasks = dbService.getAllTasks();
-        Task resultSaveTask = dbService.saveTask(task1);
-        Optional<Task> resultGetTask = dbService.getTask((long)2);
-
-        //Then
-        Assert.assertEquals(3, resultGetTasks.size());
-        Assert.assertSame(task1, resultSaveTask);
-        Assert.assertNull(resultGetTask);
-
-    }
 }
