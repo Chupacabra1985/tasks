b6e453f1d1ca90d677d768f41a56f0b873b67138
“Chupacabra1985 <jan.roztropinski@op.pl>
2018-03-17 12:49:31 +0100

    thymselfVariable

--- src/test/java/com/crud/tasks/controller/ControllerTestSuite.java
+++ src/test/java/com/crud/tasks/controller/ControllerTestSuite.java
@@ -1,28 +1,17 @@
 package com.crud.tasks.controller;
 
-import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
-import com.crud.tasks.repository.TaskRepository;
-import com.crud.tasks.service.DbService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ControllerTestSuite {
+
     @Autowired
     private TaskController taskController;
 
@@ -32,7 +21,7 @@ public class ControllerTestSuite {
         TaskDto readTask = taskController.getTask((long)2);
         long id = readTask.getId();
 
-        Assert.assertEquals(id, 2);
+        Assert.assertEquals(id, 2L);
 
     }
 
